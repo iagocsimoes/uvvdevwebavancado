cb8725b78e43c6ef12db681349dadd61d5590a2a
Simoes <simoes@MacBook-Pro-de-Simoes.local>
2024-08-06 10:39:37 -0300

    add express

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
-function soma (x: number, y: number) : number {
-  return x + y;
+import express, { Request, Response } from 'express';
 
-}
+const app = express();
 
-console.log(soma(1, 2));
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+    }
+);
