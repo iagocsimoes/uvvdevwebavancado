b323893e19440eeda7e193e15dde939aaf9e508e
iagocsimoes <iago.simoes@uvvnet.com.br>
2024-08-20 10:45:34 -0300

    add: post user

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,56 @@
 import express, { Request, Response } from "express";
+
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World");
+app.get("/", function (req: Request, res: Response) {
+  res.send("Olá pessoal, tudo bem? Quero fazer mais uma mudança");
 });
 
-app.get("/users", async (req: Request, res: Response) => {
+//Inserir usuários
+app.post("/user", async function (req: Request, res: Response) {
+  //Inserir um usuário ...
+  try {
+    const userdata = req.body;
+    console.log(userdata);
+    const newuser = await prisma.user.create({
+      data: userdata,
+    });
+
+    console.log(newuser);
+
+    res.json({
+      status: 200,
+      newuser: newuser,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      status: 500,
+      message: error,
+    });
+  }
+});
+
+// Listar usuários
+app.get("/users", async function (req: Request, res: Response) {
   const users = await prisma.user.findMany();
+
   res.json(users);
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+// Atualizar usuários
+
+app.put("/user", async function () {});
+
+// Deletar usuários
+
+app.delete("/user", async function () {});
+
+app.listen(3000, function () {
+  console.log("Servidor rodando na porta 3000");
 });
